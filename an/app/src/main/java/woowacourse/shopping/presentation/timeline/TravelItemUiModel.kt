83cb05351e18b85b9f604ed5b25711d8b8b0766b
Junyoung-WON <wjyo60@gmail.com>
2024-07-16 02:02:04 +0900

    feat: xml 파일 작성 및 UI모델 생성

---
+++ an/app/src/main/java/woowacourse/shopping/presentation/timeline/TravelItemUiModel.kt
@@ -0,0 +1,8 @@
+package woowacourse.shopping.presentation.timeline
+
+data class TravelItemUiModel(
+    val travelId: Long,
+    val travelTitle: String,
+    val travelThumbnail: String,
+    val travelDateRange: String,
+)
