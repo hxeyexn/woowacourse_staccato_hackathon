18b04630df37ab378aa1b9bc7a6986d9e660c867
hxeyexn <kck85075@kumoh.ac.kr>
2024-07-16 03:57:16 +0900

    feat: 함께 간 사람들 adapter 연결

--- an/app/src/main/java/woowacourse/shopping/presentation/traveldetail/TravelDetailActivity.kt
+++ an/app/src/main/java/woowacourse/shopping/presentation/traveldetail/TravelDetailActivity.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityTravelDetailBinding
 import woowacourse.shopping.presentation.BindingActivity
+import woowacourse.shopping.presentation.traveldetail.adapter.MatesAdapter
+import woowacourse.shopping.presentation.traveldetail.model.dummyMates
 
 class TravelDetailActivity : BindingActivity<ActivityTravelDetailBinding>() {
     override val layoutResourceId: Int
         get() = R.layout.activity_travel_detail
 
+    private lateinit var matesAdapter: MatesAdapter
+
     override fun initStartView(savedInstanceState: Bundle?) {
+        initMatesAdapter()
+    }
+
+    private fun initMatesAdapter() {
+        matesAdapter = MatesAdapter()
+        binding.rvTravelDetailMates.adapter = matesAdapter
+        matesAdapter.updateMates(dummyMates)
     }
 }
