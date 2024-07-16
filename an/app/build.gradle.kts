c1ad8f83f928667fddb6c3667605473a096b48bb
Junyoung-WON <wjyo60@gmail.com>
2024-07-16 11:23:06 +0900

    build: retrofit 관련 의존성 추가

--- an/app/build.gradle.kts
+++ an/app/build.gradle.kts
@@ -82,5 +82,8 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.9.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation ("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
 }
