cfe63af150466e57cbc851e1becd74699626c335
Junyoung-WON <wjyo60@gmail.com>
2024-07-16 11:23:38 +0900

    build: retrofit 객체 생성

--- an/app/src/main/java/woowacourse/shopping/data/StaccatoClient.kt
+++ an/app/src/main/java/woowacourse/shopping/data/StaccatoClient.kt
@@ -1,7 +1,34 @@
 package woowacourse.shopping.data
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
 import woowacourse.shopping.BuildConfig
 
 object StaccatoClient {
     private const val BASE_URL = BuildConfig.BASE_URL
+
+    private val provideLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val provideHttpClient = OkHttpClient.Builder()
+        .addInterceptor(provideLoggingInterceptor)
+        .build()
+
+    private val provideRetrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(provideHttpClient)
+        .addConverterFactory(
+            Json { ignoreUnknownKeys = true }
+                .asConverterFactory("application/json".toMediaType())
+        )
+        .build()
+
+    fun <T> create(service: Class<T>): T {
+        return provideRetrofit.create(service)
+    }
 }
