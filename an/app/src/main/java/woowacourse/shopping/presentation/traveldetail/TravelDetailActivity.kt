c5df5ab20518892175283f8dae3124ffb858aa4d
hxeyexn <kck85075@kumoh.ac.kr>
2024-07-16 12:26:45 +0900

    feat: 함께 간 사람들 더미데이터 추가

--- an/app/src/main/java/woowacourse/shopping/presentation/traveldetail/TravelDetailActivity.kt
+++ an/app/src/main/java/woowacourse/shopping/presentation/traveldetail/TravelDetailActivity.kt
@@ -1,11 +1,14 @@
 package woowacourse.shopping.presentation.traveldetail
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import woowacourse.shopping.R
 import woowacourse.shopping.databinding.ActivityTravelDetailBinding
 import woowacourse.shopping.presentation.BindingActivity
 import woowacourse.shopping.presentation.traveldetail.adapter.MatesAdapter
+import woowacourse.shopping.presentation.traveldetail.adapter.VisitsAdapter
 import woowacourse.shopping.presentation.traveldetail.model.dummyMates
+import woowacourse.shopping.presentation.traveldetail.model.dummyVisits
 
 class TravelDetailActivity : BindingActivity<ActivityTravelDetailBinding>() {
     override val layoutResourceId: Int
@@ -14,10 +17,20 @@ class TravelDetailActivity : BindingActivity<ActivityTravelDetailBinding>() {
     private lateinit var matesAdapter: MatesAdapter
     private lateinit var visitsAdapter: VisitsAdapter
 
+    private val travelDetailViewModel: TravelDetailViewModel by viewModels {
+        TravelDetailViewModelFactory()
+    }
+
     override fun initStartView(savedInstanceState: Bundle?) {
+        initBinding()
         initMatesAdapter()
         initVisitsAdapter()
         travelDetailViewModel.loadTravelDetail()
+    }
+
+    private fun initBinding() {
+        binding.lifecycleOwner = this
+        binding.viewModel = travelDetailViewModel
     }
 
     private fun initMatesAdapter() {
