0b939fa31a431a7e60b0b7d5fafbc5289db59c4b
Junyoung-WON <wjyo60@gmail.com>
2024-07-16 09:12:59 +0900

    feat: 다른 썸네일 이미지 테스트

--- an/app/src/main/java/woowacourse/shopping/presentation/timeline/TimelineAdapter.kt
+++ an/app/src/main/java/woowacourse/shopping/presentation/timeline/TimelineAdapter.kt
@@ -16,13 +16,13 @@ class TimelineAdapter: RecyclerView.Adapter<TravelViewHolder>() {
         TravelItemUiModel(
             1,
             "우테코 부산 여행",
-            "https://www.kkday.com/ko/blog/wp-content/uploads/korea_jeju_rainbow_coastal_road_4.jpg",
+            "https://avatars.githubusercontent.com/u/92203597?v=4",
             "2024.06.17 - 21"
         ),
         TravelItemUiModel(
             2,
             "서울 망원 한강 공원",
-            "https://www.kkday.com/ko/blog/wp-content/uploads/korea_jeju_rainbow_coastal_road_4.jpg",
+            "https://file.notion.so/f/f/ba7d3f51-9fa2-4951-a5b4-cd258aa3a16f/bc36390b-60b5-4f66-bce4-4969ee4b97e1/profile.png?id=f5dff214-2f30-4c24-939b-7e44140c9678&table=block&spaceId=ba7d3f51-9fa2-4951-a5b4-cd258aa3a16f&expirationTimestamp=1721160000000&signature=BFks47qrP7y0Kd_5cHl9G-Yjftpt6oXdJwYbMmPctcg&downloadName=profile.png",
             "2024.06.15"
         ),
         TravelItemUiModel(
@@ -34,7 +34,7 @@ class TimelineAdapter: RecyclerView.Adapter<TravelViewHolder>() {
         TravelItemUiModel(
             4,
             "서울 망원 한강 공원",
-            "https://www.kkday.com/ko/blog/wp-content/uploads/korea_jeju_rainbow_coastal_road_4.jpg",
+            "https://file.notion.so/f/f/ba7d3f51-9fa2-4951-a5b4-cd258aa3a16f/bc36390b-60b5-4f66-bce4-4969ee4b97e1/profile.png?id=f5dff214-2f30-4c24-939b-7e44140c9678&table=block&spaceId=ba7d3f51-9fa2-4951-a5b4-cd258aa3a16f&expirationTimestamp=1721160000000&signature=BFks47qrP7y0Kd_5cHl9G-Yjftpt6oXdJwYbMmPctcg&downloadName=profile.png",
             "2024.06.15"
         ),
         TravelItemUiModel(
